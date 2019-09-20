dc7dd29baf1ab10f9dcc10ed6f1ea059e8ebd75e
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-20 05:00:34 -0500

    create renderMovies function

--- src/index.js
+++ src/index.js
@@ -20,3 +20,42 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+//DYNAMICALLY CREATES HTML BASED ON WHAT IS IN db.json
+
+//BOOTSTRAP CARD REFERENCE
+
+// <div class="card" style="width: 18rem;">
+//     <div class="card-body">
+//     <h5 class="card-title">Card title</h5>
+// <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+// <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+// <a href="#" class="card-link">Card link</a>
+// <a href="#" class="card-link">Another link</a>
+// </div>
+// </div>
+
+function renderMovies(movie) {
+
+  //declare empty variable to hold HTML that is being dynamically created
+
+  let dynamicHTML = "";
+
+  dynamicHTML = `
+  
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<a href="#" class="card-link">Card link</a>
+<a href="#" class="card-link">Another link</a>
+</div>
+</div>
+  
+  
+  `
+
+
+}
