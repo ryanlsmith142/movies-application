d719ab268783941bbe2dc37d1d40ba9adfe3ead9
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-20 05:14:26 -0500

    renderHTML function now renders html onto page in bootstrap card format

--- src/index.js
+++ src/index.js
@@ -13,11 +13,14 @@ const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
+  let dynamicHTML = "";
   movies.forEach(({title, rating, id}) => {
     // console.log(`id#${id} - ${title} - rating: ${rating}`);
-    console.log(renderMovies(title, rating, id));;
+    dynamicHTML += renderMovies(title, rating, id);
 
   });
+
+  $(".container").html(dynamicHTML);
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
