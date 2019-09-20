76db883b50629ca51cff24292df1fc98f3cc0e66
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-20 09:06:07 -0600

    add add-movie html and function

--- src/api.js
+++ src/api.js
@@ -4,3 +4,34 @@ module.exports = {
       .then(response => response.json());
   }
 };
+
+module.exports = {
+  addMovie: () => {
+    return fetch('/api/movies/').then(postMovie () => {
+          let movieTitle = $('#new-coffee').val();
+          let movieRating = $('#new-movie-rating').val();
+          let newMovie = {
+            title: movieTitle,
+            rating: movieRating
+          };
+          const url = '/api/movies';
+          const options = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovie),
+          };
+          // fetch(url, options)
+          //     .then(renderMovies());
+          //     .catch(console.log("movie didn't load"));
+
+        });
+
+
+
+
+
+  }
+
+};
--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@
 
 const {getMovies} = require('./api.js');
 
+const {addMovie} = require('./api.js');
+
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
@@ -29,18 +31,6 @@ getMovies().then((movies) => {
 
 //DYNAMICALLY CREATES HTML BASED ON WHAT IS IN db.json
 
-//BOOTSTRAP CARD REFERENCE
-
-// <div class="card" style="width: 18rem;">
-//     <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-// <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-// <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-// <a href="#" class="card-link">Card link</a>
-// <a href="#" class="card-link">Another link</a>
-// </div>
-// </div>
-
 function renderMovies(title,rating, id) {
 
   //declare empty variable to hold HTML that is being dynamically created
@@ -59,3 +49,6 @@ function renderMovies(title,rating, id) {
   return dynamicHTML;
 
 }
+
+$('#add-movie').on("click", addMovie);
+
