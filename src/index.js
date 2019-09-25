12d118c4ccdf862e4eb031745b1d48bed7c0a096
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-25 09:03:29 -0500

    add edit button html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 /**
  * es6 modules and imports
  */
-
 /**
  * require style imports
  */
+
+
 function getMovies() {
     console.log("getmovies ran");
   return fetch('/api/movies')
@@ -28,7 +29,7 @@ function addMovie() {
     body: JSON.stringify(newMovie),
   };
   fetch(url, options)
-      .then()
+      .then(renderMovies())
       .catch(function() {
           console.log("Hey we couldn't add a movie or update the movies.")
       });
@@ -38,7 +39,6 @@ function addMovie() {
 getMovies().then((movies) => {
   let dynamicHTML = "";
   movies.forEach(({title, rating, id}) => {
-    // console.log(`id#${id} - ${title} - rating: ${rating}`);
     dynamicHTML += renderMovies(title, rating, id);
 
   });
@@ -63,6 +63,7 @@ function renderMovies(title,rating, id) {
       <div class="card-body">
       <h5 class="card-title">${title}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${rating}</h6>
+      <button id="edit-movie" type="button" class="btn btn-secondary">Edit</button>
       </div>
     </div>
   `;
@@ -73,10 +74,6 @@ function renderMovies(title,rating, id) {
 
 //ADDS A MOVIE TO DATABASE ON CLICK
 
-$('#add-movie-btn').on("click", function(e) {
-  // e.preventDefault();
-  console.log("click event occured");
-  addMovie();
-});
+$('#add-movie-btn').on("click", addMovie());
 
-
+$('#edit-movie').on("click",)
